d559f8ca122641f24b9deb8a8b5834488e2b80b9
kabusdevvvv <145866076+kabusdevvvv@users.noreply.github.com>
2023-09-24 16:44:54 +0200

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,1 +1,1 @@
-{"BETA":{"Prizes":["BLOOD_FX_CRATE","BLOOD_FX_CRATE"],"Time":["Sep 26, 2023"]},"12345":{"Prizes":["TAG_CRATE","TAG_CRATE"],"Time":["Sep 26, 2023"]}}
+{"BETA":{"Prizes":["BLOOD_FX_CRATE","BLOOD_FX_CRATE"],"Time":["Sep 25, 2023"]},"TESTER":{"Prizes":["TAG_FX_CRATE","TAG_CRATE","TAG_FX_CRATE"],"Time":["Sep 25, 2023"]},"123456":{"Prizes":["TAG_CRATE","TAG_CRATE","BLOOD_FX_CRATE"],"Time":["Sep 25, 2023"]},"123":{"Prizes":["BLOOD_FX_CRATE","BLOOD_FX_CRATE", "BLOOD_FX_CRATE"],"Time":["Sep 23, 2023"]}}
